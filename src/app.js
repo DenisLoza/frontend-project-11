dd29169dc4f97b815cbff4d702110b3de6fc4103
Denis Loza <denisloza.git@gmail.com>
2023-03-09 12:54:55 +0200

    Step 4. upd ru.js, app.js, view.js, add parser.js

--- src/app.js
+++ src/app.js
@@ -1,24 +1,56 @@
+import axios from 'axios';
 import i18next from 'i18next';
 import onChange from 'on-change';
 import { string, setLocale } from 'yup';
+import rawXMLparser from './parser.js';
 import {
-  renderRssForm,
+  renderRssFormFeedback,
   renderRssFormError,
+  renderPostsCard,
+  renderFeedsCard,
 } from './view.js';
 import resources from './locales/index.js';
 
-const validateUrl = (state, url) => {
+const validateURL = (state, url) => {
   const schema = string().url().notOneOf(state.channels);
   return schema.validate(url);
 };
 
-const runApp = (initialState, elements, i18n) => {
-  const state = onChange(initialState, (path, value) => { // previousValue
+const getFeed = (initialState, state, url) => {
+  axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`)
+    .then((response) => {
+      if (response.data.status.http_code === 200) {
+        state.rssForm.status = 'downloaded';
+        return response.data.contents;
+      }
+      throw new Error('networkResponseError');
+    })
+    .then((rawXML) => {
+      const { parsedFeed, parsedPosts } = rawXMLparser(rawXML);
+      const isNewFeed = state.feeds.every(({ title }) => title !== parsedFeed.title);
+      if (isNewFeed) state.feeds.push(parsedFeed);
+
+      state.posts = parsedPosts;
+    })
+    .catch((err) => {
+      state.rssForm.errors = err;
+    });
+};
+
+const createWatchedState = (initialState, elements, i18n) => {
+  const state = onChange(initialState, (path, value) => {
     switch (path) {
-      case 'rssForm.status':
-        renderRssForm(value, elements);
-        break;
       case 'channels':
+        getFeed(initialState, state, value.at(-1));
+        break;
+      case 'posts':
+        renderPostsCard(value, elements, i18n);
+        break;
+      case 'feeds':
+        renderFeedsCard(value, elements, i18n);
+        break;
+      case 'rssForm.status':
+        renderRssFormFeedback(value, elements, i18n);
         break;
       case 'rssForm.errors':
         renderRssFormError(value, elements, i18n);
@@ -27,22 +59,27 @@ const runApp = (initialState, elements, i18n) => {
         throw new Error(`Unknown state path: ${path}`);
     }
   });
+  return state;
+};
 
-  const { form } = elements;
+const runApp = (initialState, elements, i18n) => {
+  const state = createWatchedState(initialState, elements, i18n);
+  const { form, input } = elements;
   form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const url = formData.get('url');
-
-    validateUrl(state, url)
+    validateURL(state, url)
       .then(() => {
-        state.rssForm.status = 'valid';
+        state.rssForm.status = 'processing';
         state.channels.push(url);
       })
       .catch((err) => {
         state.rssForm.status = 'invalid';
         state.rssForm.errors = err;
       });
+    form.reset();
+    input.focus();
   });
 };
 
@@ -65,6 +102,8 @@ const initApp = () => {
       });
       const initialState = {
         channels: [],
+        feeds: [],
+        posts: [],
         rssForm: {
           status: 'invalid',
           errors: [],
@@ -73,9 +112,11 @@ const initApp = () => {
       const elements = {
         form: document.querySelector('form'),
         input: document.getElementById('url-input'),
+        formSubmitButton: document.querySelector('.btn-primary'),
         feedback: document.querySelector('.feedback'),
+        postsContainer: document.querySelector('.posts'),
+        feedsContainer: document.querySelector('.feeds'),
       };
-      // const state = view(initialState, elements, i18n);
 
       runApp(initialState, elements, i18n);
     })
--- src/view.js
+++ src/view.js
@@ -1,25 +1,105 @@
-const renderRssFormError = (error, elements, i18n) => {
-  const { feedback } = elements;
-  feedback.innerHTML = i18n.t(error.message);
-  feedback.classList.add('text-danger');
-  feedback.classList.remove('text-success');
+const makeLinkVisited = (event) => {
+  event.target.classList.remove('fw-bold');
+  event.target.classList.add('fw-normal', 'link-secondary');
 };
 
-const renderRssForm = (status, elements) => {
-  const { form, input, feedback } = elements;
+const createCardAndTitle = (titleTranslation, i18n) => {
+  const card = document.createElement('div');
+  card.classList.add('card', 'border-0');
+
+  const cardBody = document.createElement('div');
+  cardBody.classList.add('card-body');
+
+  const cardTitle = document.createElement('h2');
+  cardTitle.classList.add('card-title', 'h4');
+  cardTitle.innerHTML = i18n.t(titleTranslation);
+
+  const elList = document.createElement('ul');
+  elList.classList.add('list-group', 'border-0', 'rounded-0');
+  cardBody.append(cardTitle);
+  card.append(cardBody);
+  return { card, elList };
+};
+
+const renderPostsCard = (postsData, elements, i18n) => {
+  const { postsContainer } = elements;
+  const { card, elList } = createCardAndTitle('postsTitle', i18n);
+  postsData.forEach((postData) => {
+    const { title, link, id } = postData;
+    const li = document.createElement('li');
+    li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
+    const a = document.createElement('a');
+    a.classList.add('fw-bold');
+    a.addEventListener('click', (e) => makeLinkVisited(e));
+    a.dataset.id = id;
+    a.innerHTML = title;
+    Object.assign(a, { href: link, target: '_blank', rel: 'noopener noreferrer' });
+    const button = document.createElement('button');
+    button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
+    button.dataset.id = id;
+    button.dataset.bsToggle = 'modal';
+    button.dataset.bsTarget = '#modal';
+    button.innerHTML = i18n.t('btnWatchMore');
+    button.setAttribute('type', 'button');
+    li.append(a, button);
+    elList.append(li);
+  });
+  card.append(elList);
+  postsContainer.replaceChildren(card);
+};
+
+const renderFeedsCard = (feedsData, elements, i18n) => {
+  const { feedsContainer } = elements;
+  const { card, elList } = createCardAndTitle('feedsTitle', i18n);
+  feedsData.forEach((feedData) => {
+    const { title, description } = feedData;
+    const li = document.createElement('li');
+    li.classList.add('list-group-item', 'border-0', 'border-end-0');
+    const h3 = document.createElement('h3');
+    h3.classList.add('h6', 'm-0');
+    h3.innerHTML = title;
+    const p = document.createElement('p');
+    p.classList.add('m-0', 'small', 'text-black-50');
+    p.innerHTML = description;
+    li.append(h3, p);
+    elList.append(li);
+  });
+  card.append(elList);
+  feedsContainer.replaceChildren(card);
+};
+
+const renderClearForm = (elements) => {
+  const { input, formSubmitButton, feedback } = elements;
+  feedback.classList.remove('text-success');
+  feedback.classList.remove('text-danger');
+  feedback.innerHTML = '';
+  input.classList.remove('is-invalid');
+  input.removeAttribute('readonly');
+  formSubmitButton.removeAttribute('disabled');
+};
+
+const renderRssFormError = (error, elements, i18n) => {
+  renderClearForm(elements);
+  const { input, feedback } = elements;
+  input.classList.add('is-invalid');
+  feedback.innerHTML = i18n.t(error.message);
+  feedback.classList.add('text-danger');
+};
+
+const renderRssFormFeedback = (status, elements, i18n) => {
+  renderClearForm(elements);
+  const { input, formSubmitButton, feedback } = elements;
   switch (status) {
     case 'invalid':
       input.classList.add('is-invalid');
       break;
-    case 'valid':
-      input.classList.remove('is-invalid');
-      // Successfull url load
-      feedback.classList.remove('text-danger');
+    case 'processing':
+      input.setAttribute('readonly', 'true');
+      formSubmitButton.setAttribute('disabled', 'disabled');
+      break;
+    case 'downloaded':
       feedback.classList.add('text-success');
-      feedback.innerHTML = 'RSS успешно загружен'; // i18n.t(rssSuccessedLoad);
-      // ^ TODO: переместить строки в блок при успешной загрузке rss
-      form.reset();
-      input.focus();
+      feedback.innerHTML = i18n.t('rssSuccessedLoad');
       break;
     default:
       throw new Error(`Unknown form render status: ${status}`);
@@ -27,6 +107,8 @@ const renderRssForm = (status, elements) => {
 };
 
 export {
-  renderRssForm,
+  renderRssFormFeedback,
   renderRssFormError,
+  renderPostsCard,
+  renderFeedsCard,
 };
